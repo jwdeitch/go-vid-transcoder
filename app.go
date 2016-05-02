afdfd2d06f64ea08c364d9d9d33aeb9807346801
jwdeitch <jwdeitch@gmail.com>
2016-05-01 23:10:19 -0400

    file uploads

--- app.go
+++ app.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"os"
+	"io"
 	"github.com/emirozer/go-helpers"
 )
 
@@ -24,15 +26,61 @@ func main() {
 	fmt.Println("We're up and running")
 
 	http.HandleFunc("/video", getVideo) // GET video
+	http.HandleFunc("/video/upload", uploadVideo) // GET video
 
 	err := http.ListenAndServe(":9090", nil)
 	helpers.Check(err)
 
 }
 
-func getVideo(w http.ResponseWriter, r *http.Request) {
-	hostname := r.URL.Query().Get("id")
+//Thanks! http://sanatgersappa.blogspot.com/2013/03/handling-multiple-file-uploads-in-go.html
+func uploadVideo(w http.ResponseWriter, r *http.Request) {
 	logVisit(r)
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	} else {
+		//parse the multipart form in the request
+		err := r.ParseMultipartForm(100000)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		//get a ref to the parsed multipart form
+		m := r.MultipartForm
+
+		//get the *fileheaders
+		files := m.File["myfiles"]
+		for i, _ := range files {
+			//for each fileheader, get a handle to the actual file
+			file, err := files[i].Open()
+			defer file.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			//create destination file making sure the path is writeable.
+			dst, err := os.Create("/tmp/" + files[i].Filename)
+			defer dst.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			//copy the uploaded file to the destination file
+			if _, err := io.Copy(dst, file); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+		}
+		fmt.Fprintf(w, "Upload successful.")
+	}
+
+}
+
+func getVideo(w http.ResponseWriter, r *http.Request) {
+	logVisit(r)
+	hostname := r.URL.Query().Get("id")
 	fmt.Fprintf(w, hostname)
 }
 
