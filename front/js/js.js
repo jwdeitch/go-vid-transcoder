b4d175757a84183c357db20025e038eb8b20ed63
jwdeitch <jwdeitch@gmail.com>
2016-07-24 13:32:39 -0400

    front end progress and player change

--- front/js/js.js
+++ front/js/js.js
@@ -12,6 +12,41 @@ $(document).ready(function () {
         });
     };
     reSplit();
+    var player = plyr.setup()[0].plyr;
 
-    plyr.setup();
+    vue = new Vue({
+        el: 'body',
+        data: {
+            videos: []
+        },
+        methods: {
+            getData: function (e) {
+                $.get('https://oizgt5pjf8.execute-api.us-east-1.amazonaws.com/prod/aws-vid-transcoder_webService').done(function (data) {
+                    vue.$set('videos', data);
+                });
+            },
+            changeVideo: function (e) {
+                var d_key = $(e.target).data('d_key');
+                var stamp = $(e.target).data('stamp');
+                player.source({
+                    type: 'video',
+                    sources: [{
+                        src: "https://s3.amazonaws.com/idrsainput/output/" + stamp + "%23" + d_key + "%23.webm",
+                        type: 'video/webm'
+                    }],
+                    poster: 'https://s3.amazonaws.com/idrsainput/output/' + stamp + '%23' + d_key + '%23_thumb00001.jpg'
+                });
+                player.play();
+            }
+        }
+
+    });
+
+    vue.getData();
+    window.setInterval(function () {
+        vue.getData();
+        console.log('Refreshed data');
+    }, 30000);
+
+
 });
