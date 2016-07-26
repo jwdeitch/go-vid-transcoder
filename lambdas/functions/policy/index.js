e8e2648c62ad7dcf5157e34daa0797483ce8f2db
Jordan Deitch <jordan.deitch@inturn.co>
2016-07-26 16:22:59 -0400

    securing filenames

--- lambdas/functions/policy/index.js
+++ lambdas/functions/policy/index.js
@@ -1,10 +1,23 @@
 exports.handle = function (e, ctx, cb) {
 
+    // For security, we want to strip all non alpha-numeric characters
     function processFilename(rawFN) {
+        if (rawFN.lastIndexOf(".") == -1) {
+            return;
+        }
 
-        extension = rawFN.substring(rawFN.lastIndexOf("."), rawFN.length);
-        filename = rawFN.substring(0, rawFN.lastIndexOf("."));
-        return filename.replace(/[^a-z0-9+]+/gi, " ").trim().replace(/ /g, "_") + "." + extension.replace(/\W/g, '');
+        extension = rawFN.substring(rawFN.lastIndexOf("."), rawFN.length).replace(/\W/g, '');
+        filename = rawFN.substring(0, rawFN.lastIndexOf(".")).replace(/[^a-z0-9+]+/gi, " ").trim().replace(/ /g, "_");
+
+        if (filename.length == 0) {
+            filename = "I_did_not_provide_a_valid_filename"
+        }
+
+        if (extension.length == 0) {
+            return false;
+        }
+
+        return filename + "." + extension;
     }
 
     var s3 = require("policyWriter");
