6a738520ee4164fd469b41595e63822c044af24b
jwdeitch <jwdeitch@gmail.com>
2016-08-08 22:51:09 -0400

    remove pagination

--- lambdas/functions/upload/main.go
+++ lambdas/functions/upload/main.go
@@ -85,7 +85,7 @@ func main() {
 		var s3Upload S3UploadedDocument
 		json.Unmarshal(eventString, &s3Upload)
 
-		insStmt, err := db.Prepare("INSERT INTO videos VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+		insStmt, err := db.Prepare("INSERT INTO videos VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 		if err != nil {
 			l.Println(err.Error())
 		}
